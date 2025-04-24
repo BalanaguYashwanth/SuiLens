07138853d14a61e00cd3982692465ffb91d757f8
BalanaguYashwanth <balanaguyashwanth@gmail.com>
2025-04-24 10:33:57 +0530

    add toods in backend

--- suilens-backend/controllers/dbquery.ts
+++ suilens-backend/controllers/dbquery.ts
@@ -6,6 +6,8 @@ type DB_QUERY = {
     query: string
 }
 
+
+
 export const dbquery = ({module, query}: DB_QUERY) => {
     const dbPath = `./db/${module}.sqlite`;
 
@@ -16,4 +18,23 @@ export const dbquery = ({module, query}: DB_QUERY) => {
     
     const row = db.prepare(query).all();
     return row
+}
+
+const classfieragent = (data: any) => {
+    //decide text or query, if it query return it 
+    // if text process to query and return it
+}
+
+export const getdata = () => {
+    
+    //TODOs
+    //multiple agents
+
+    // trigger first classifier to decide text or query 
+    // once data you got then decide which chart it should display
+
+    const query = classfieragent(data)
+    //trigger the data dbquery
+
+
 }
