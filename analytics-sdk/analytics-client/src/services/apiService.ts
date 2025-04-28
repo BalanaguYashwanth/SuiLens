95cafcc2758fc8272f5b866c2102e90b3a078054
Saurabh Pingale <saurabhpingale93@gmail.com>
2025-04-28 15:53:33 +0530

    Added route to get the API URL

--- analytics-sdk/analytics-client/src/services/apiService.ts
+++ analytics-sdk/analytics-client/src/services/apiService.ts
@@ -12,7 +12,7 @@ const api = axios.create({
 export async function createDatabaseAndTable(dbName: string, tableName: string): Promise<void> {
   if (!dbName) throw new ValidationError('Database name are required.');
   try {
-    const res = await api.post('/database/create', { dbName, tableName });
+    const res = await api.post('/event/create', { dbName, tableName });
     if (res.status !== 200) throw new Error('Unexpected response while creating DB/Table.');
   } catch (error: any) {
     console.error('Error during createDatabaseAndTable:', error.message);
@@ -23,7 +23,7 @@ export async function createDatabaseAndTable(dbName: string, tableName: string):
 export async function insertTable(dbName: string, tableName: string, data: any): Promise<void> {
   if (!dbName || !tableName || !data) throw new ValidationError('Database, table, and data are required for insert.');
   try {
-    const res = await api.post('/database/insert', { dbName, tableName, data });
+    const res = await api.post('/event/insert', { dbName, tableName, data });
     if (res.status !== 200) throw new Error('Unexpected response while inserting.');
   } catch (error: any) {
     console.error('Error during insertData:', error.message);
@@ -34,7 +34,7 @@ export async function insertTable(dbName: string, tableName: string, data: any):
 export async function updateTable(dbName: string, tableName: string, data: any): Promise<void> {
   if (!dbName || !tableName || !data) throw new ValidationError('Database, table, and data are required for update.');
   try {
-    const res = await api.put('/database/update', { dbName, tableName, data });
+    const res = await api.put('/event/update', { dbName, tableName, data });
     if (res.status !== 200) throw new Error('Unexpected response while updating.');
   } catch (error: any) {
     console.error('Error during updateData:', error.message);
@@ -45,7 +45,7 @@ export async function updateTable(dbName: string, tableName: string, data: any):
 export async function deleteTable(dbName: string, tableName: string): Promise<void> {
   if (!dbName || !tableName) throw new ValidationError('Database and table are required for delete.');
   try {
-    const res = await api.delete('/database/delete', { data: { dbName, tableName } });
+    const res = await api.delete('/event/delete', { data: { dbName, tableName } });
     if (res.status !== 200) throw new Error('Unexpected response while deleting table.');
   } catch (error: any) {
     console.error('Error during deleteTable:', error.message);
--- analytics-sdk/analytics-storage-server/app/routes/__init__.py
+++ analytics-sdk/analytics-storage-server/app/routes/__init__.py
@@ -1,6 +1,8 @@
 from app.custom_fastapi import CustmFastAPI
 
 from app.routes.event import event_router
+from app.routes.api_url import api_url_router
 
 def init_routes(app: CustmFastAPI):
     app.include_router(event_router)
+    app.include_router(api_url_router)
