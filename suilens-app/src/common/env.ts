35126393224c28bfe66a8099ce3fd7014f4e2a2f
saurabh-pingale <saurabh.pingale@formpilot.org>
2025-05-21 10:36:05 +0530

    Replace Backed URL

--- suilens-app/src/common/env.ts
+++ suilens-app/src/common/env.ts
@@ -1,3 +1,3 @@
-export const BACKEND_API = 'http://localhost:8002'
-export const MCP_CLIENT_API = 'http://localhost:8000'
+export const BACKEND_API = 'https://lensapi.formpilot.org/server'
+export const MCP_CLIENT_API = 'https://lensapi.formpilot.org/client'
 export const SUILENS_ANALYTICS_BACKEND_API = 'https://lensapi.formpilot.org'
